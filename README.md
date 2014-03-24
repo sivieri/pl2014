f4baef53adf31e4f3308d454df78a4033ed3c76a
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2014-03-24 12:08:27 +0100

    Corrections; added readme.

---
+++ README.md
@@ -0,0 +1,9 @@
+Exercises for Principles of Programming Languages
+==========================
+
+This repository contains all the source code for the exercise lectures for the 2014 edition of the *Principles of Programming Languages* course. Please refer to the [course site](http://home.deib.polimi.it/pradella/PL.html) for the lessons material.
+
+# Lecture 1
+* Basic scheme programming: if, cond, named let
+* Recursion and tail recursion
+* Lists
--- es1/es1.rkt
+++ es1/es1.rkt
@@ -14,6 +14,22 @@
       0
       (+ 1 (len (cdr l)))))
 
+; List length (tail recursive)
+(define (len2 l)
+  (define (len2-helper in acc)
+    (if (null? in)
+        acc
+        (len2-helper (cdr in) (+ 1 acc))))
+  (len2-helper l 0))
+
+; List length (named let)
+(define (len3 l)
+  (let len3-helper ((in l)
+                    (acc 0))
+    (if (null? in)
+        acc
+        (len3-helper (cdr in) (+ 1 acc)))))
+
 ; Range function
 (define (range lo hi)
   (if (> lo hi)
@@ -27,24 +43,6 @@
   (if (< lo hi)
       (cons lo (rangestep (+ step lo) hi step))
       '()))
-
-; Compress
-(define (compress head . tail)
-  (if (null? tail)
-      head
-      (if (eqv? head (car tail))
-          (compress tail)
-          (cons head (compress tail)))))
-
-; Compress (tail recursive)
-(define (compresstr head . tail)
-  (define (compress-helper in acc last)
-    (if (null? in)
-        acc
-        (if (eqv? (car in) last)
-            (compress-helper (cdr in) acc last)
-            (compress-helper (cdr in) (cons (car in) acc) (car in)))))
-  (reverse (compress-helper tail (list head) head)))
 
 ; Fibonacci
 (define (fib n)
@@ -68,19 +66,7 @@
       '()
       (cons (f (car l)) (my-map f (cdr l)))))
 
-; Exercises:
-; 1. Rewrite the map implementation using a named let
-; 2. Write the function "takewhile", which takes 2 elements (a function and
-;    a list) and returns the longest prefix of the list for which the
-;    function returns "true"
-; 3. Write a function that takes successive equal elements from a list and 
-;    puts them into a sublist
-;    e.g., (1 1 1 2 3 3 4) -> ((1 1 1) (2) (3 3) (4))
-; 4. Use the previous function to encode a list: each sequence of equal elements
-;    is converted into a list of two elements, the symbol and the number of instances
-;    e.g., (1 1 1 2 3 3 4) -> ((3 1) (1 2) (2 3) (1 4))
-
-; Ex. 1
+; Map (named let)
 (define (my-map2 f l)
   (let my-map2-helper ((fun f)
                        (in l))
@@ -88,13 +74,16 @@
         '()
         (cons (fun (car in)) (my-map2-helper fun (cdr in))))))
 
-; Ex. 2
+; Takewhile (returns the longest prefix of the list for which the function
+; given as a parameter returns true)
+; e.g., (2 4 6 7 8 9), (lambda (x) (= (remainder x 2) 0)) -> (2 4 6)
 (define (takewhile f l)
   (if (f (car l))
       (cons (car l) (takewhile f (cdr l)))
       '()))
 
-; Ex. 3
+; Pack (put together in sublists successions of equal elements)
+; e.g., (1 1 1 2 3 3 4) -> ((1 1 1) (2) (3 3) (4))
 (define (pack l)
   (define (pack-helper in acc sub)
     (cond ((null? in) (cons sub acc))
@@ -103,7 +92,8 @@
           (else (pack-helper (cdr in) (cons sub acc) (list (car in))))))
   (reverse (pack-helper l '() '())))
 
-; Ex. 4
+; Encode (substitute successions of equal elements with the length of the successions)
+; e.g., (1 1 1 2 3 3 4) -> ((3 1) (1 2) (2 3) (1 4))
 (define (encode l)
   (define (pack-helper packed)
     (if (null? packed)
