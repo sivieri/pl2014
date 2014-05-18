4c265b185ecdafaace524e10cdabfe3179b402ea
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2014-05-18 20:34:48 +0200

    Working on the fifth.

---
+++ es5/Es5.hs
@@ -0,0 +1,134 @@
+module Es5 where
+
+import qualified Data.Map as M
+import Control.Monad
+
+-- Folds
+myReverse :: [a] -> [a]
+myReverse = foldl (\acc x -> x:acc) []
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter f = foldr (\x acc -> if f x then x:acc else acc) []
+
+-- Binary trees
+-- let nums = [8,6,4,1,7,3,5]
+-- let numsTree = foldr treeInsert EmptyTree nums
+-- (foldr because treeInsert takes the current tree as second parameter)
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a = Node a (treeInsert x left) right
+    | x > a = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+    | x == a = True
+    | x < a = treeElem x left
+    | x > a = treeElem x right
+
+treeSum :: Num a => Tree a -> a
+treeSum EmptyTree = 0
+treeSum (Node a left right) = a + (treeSum left) + (treeSum right)
+
+treeValues :: Tree a -> [a]
+treeValues EmptyTree = []
+treeValues (Node a left right) = a : ((treeValues left) ++ (treeValues right))
+
+-- Monads
+-- class Monad m where
+--     return :: a -> m a
+--     (>>=) :: m a -> (a -> m b) -> m b
+--     (>>) :: m a -> m b -> m b
+--     x >> y = x >>= \_ -> y
+--     fail :: String -> m a
+--     fail msg = error msg
+
+-- Maybe monad
+-- instance Monad Maybe where
+--     return x = Just x
+--     Nothing >>= f = Nothing
+--     Just x >>= f  = f x
+--     fail _ = Nothing
+
+foo :: (Show a, Show b) => Maybe a -> Maybe b -> Maybe String  
+foo a b = do  
+    x <- a
+    y <- b
+    Just (show x ++ show y)
+
+-- k-v map monad
+type PersonName = String
+type PhoneNumber = String
+type BillingAddress = String
+data MobileCarrier = Honest_Bobs_Phone_Network
+                   | Morrisas_Marvelous_Mobiles
+                   | Petes_Plutocratic_Phones
+                     deriving (Eq, Ord)
+
+findCarrierBillingAddress :: PersonName
+                          -> M.Map PersonName PhoneNumber
+                          -> M.Map PhoneNumber MobileCarrier
+                          -> M.Map MobileCarrier BillingAddress
+                          -> Maybe BillingAddress
+findCarrierBillingAddress person phoneMap carrierMap addressMap =
+    case M.lookup person phoneMap of
+      Nothing -> Nothing
+      Just number ->
+          case M.lookup number carrierMap of
+            Nothing -> Nothing
+            Just carrier -> M.lookup carrier addressMap
+
+findCarrierBillingAddress2 :: PersonName
+                          -> M.Map PersonName PhoneNumber
+                          -> M.Map PhoneNumber MobileCarrier
+                          -> M.Map MobileCarrier BillingAddress
+                          -> Maybe BillingAddress
+findCarrierBillingAddress2 person phoneMap carrierMap addressMap = do
+    number <- M.lookup person phoneMap
+    carrier <- M.lookup number carrierMap
+    M.lookup carrier addressMap
+
+-- Logger monad
+type Log = [String]
+newtype Logger a = Logger { execLogger :: (a, Log) }
+
+instance Functor Logger where
+    fmap f m = Logger (f a)
+        where (a, _) = execLogger m
+
+instance Monad Logger where
+    return a = Logger (a, [])
+    m >>= k = let (a, w) = execLogger m
+                  n      = k a
+                  (b, x) = execLogger n
+              in Logger (b, w ++ x)
+
+instance Show a => Show (Logger a) where
+    show (Logger a) = show a
+
+runLogger :: Logger a -> (a, Log)
+runLogger = execLogger
+
+record :: String -> Logger ()
+record s = Logger ((), [s])
+
+singletonM :: (Show a) => a -> Logger (Tree a)
+singletonM x = record ("Created singleton " ++ show x) >> return (Node x EmptyTree EmptyTree)
+
+treeInsertM :: (Ord a, Show a) => Tree a -> a -> Logger (Tree a)
+treeInsertM EmptyTree x = singletonM x
+treeInsertM (Node a left right) x
+    | x == a = record ("Inserted " ++ show x) >> return (Node x left right)
+    | x < a = do
+        l <- treeInsertM left x
+        return (Node a l right)
+    | x > a = do
+        r <- treeInsertM right x
+        return (Node a left r)
