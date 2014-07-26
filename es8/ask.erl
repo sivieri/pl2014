3fa2f35f82d6f74aa452dda0856db670620d3361
Siv <alessandro.sivieri@gmail.com>
2014-07-26 21:00:12 +0200

    Correction.

--- es8/ask.erl
+++ es8/ask.erl
@@ -4,16 +4,16 @@
 % Public API
 
 % erl -sname test1 -setcookie abc
-% es8:start_ask().
+% ask:start_ask().
 start_ask() -> 
     Pid = spawn(fun loop_ask/0),
     register(server, Pid),
     on_exit_ask(Pid, fun start_ask/0).
 
 % erl -sname test2 -setcookie abc
-% es8:ask(test1@hostname, fun(X) -> X * X end, 8).
-% es8:ask(test1@hostname, fun(X) -> X * X end, aaa).
-% es8:ask(test1@hostname, fun(X) -> X * X end, 8).
+% ask:ask(test1@hostname, fun(X) -> X * X end, 8).
+% ask:ask(test1@hostname, fun(X) -> X * X end, aaa).
+% ask:ask(test1@hostname, fun(X) -> X * X end, 8).
 ask(Host, Fun, Num) ->
     {server, Host} ! {self(), Fun, Num},
     receive
